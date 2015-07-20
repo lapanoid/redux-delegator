e8f9426d640e7d5690342237050cc539aa780a17
Sergey Lapin <lapanoid@gmail.com>
2015-07-20 12:10:35 +0300

    rename store to reducer

--- src/index.js
+++ src/index.js
@@ -3,27 +3,25 @@ import Immutable from 'immutable'
 import reduceReducers from 'reduce-reducers'
 import { isError } from 'flux-standard-action';
 
-function handleActionsOnReducers(reducers, defaultState) {
-    return typeof defaultState !== 'undefined'
+const handleActionsOnReducers = (reducers, defaultState) =>(
+    typeof defaultState !== 'undefined'
         ? (state = defaultState, action) => reduceReducers(...reducers)(state, action)
-        : reduceReducers(...reducers);
-}
+        : reduceReducers(...reducers)
+);
 
-function getStoreDefault(store) {
-    return store(undefined, {type: null})
-}
+const getReducerDefault = (reducer) =>  reducer(undefined, {type: null});
 
-export const createImmutableDelegator = (propStoreList)=> {
-    const reduce = R.reduce(({reducers, defaultObj}, {prop, store})=> {
+export const createImmutableDelegator = (propReducerList)=> {
+    const reduce = R.reduce(({reducers, defaultObj}, {prop, reducer})=> {
         reducers.push(
-            (state, action)=> state.set(prop, store(state.get(prop), action))
+            (state, action)=> state.set(prop, reducer(state.get(prop), action))
         );
 
-        defaultObj[prop] = getStoreDefault(store);
+        defaultObj[prop] = getReducerDefault(reducer);
         return {reducers, defaultObj}
     }, {reducers: [], defaultObj: {}});
 
-    const {reducers, defaultObj} = reduce(propStoreList);
+    const {reducers, defaultObj} = reduce(propReducerList);
 
     return handleActionsOnReducers(
         reducers,
